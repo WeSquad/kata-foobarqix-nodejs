cfa7e122ec5b4ac4d14cc39ccce605ad2d99ca77
Jimmy Grande <jimmy.grande@outlook.com>
2020-04-23 14:32:46 +0200

    should return FooBarBar when number contains 3 and 5 AND is divisible by 5 and 7 : test ok

--- index.spec.js
+++ index.spec.js
@@ -56,27 +56,33 @@ describe('Implement the divide rule', () => {
 })
 
 describe('Implement the `contains` rule with the divide rule', () => {
-    it('should return Foo if value contains 3 and has no divide rule', () => {
+    it('should return Foo if value contains 3 AND has no divide rule', () => {
         const input = '13'
         const output = 'Foo'
         expect(compute(input)).toEqual(output)        
     })
 
-    it('should return FooFoo if value contains 3 and is divisible by 3', () => {
+    it('should return FooFoo if value contains 3 AND is divisible by 3', () => {
         const input = '3'
         const output = 'FooFoo'
         expect(compute(input)).toEqual(output)        
     })
 
-    it('should return BarBar if value contains 5 and has no divide rule', () => {
+    it('should return BarBar if value contains 5 AND is divisible by 5', () => {
         const input = '5'
         const output = 'BarBar'
         expect(compute(input)).toEqual(output)        
     })
 
-    it('should return FooBarBar if value contains 5 and has no divide rule', () => {
+    it('should return FooBarBar if value contains 5 AND is divisible by 3 and 5', () => {
         const input = '15'
         const output = 'FooBarBar'
+        expect(compute(input)).toEqual(output)        
+    })
+
+    it('should return BarQixFooBar if value contains 3 and 5 AND is divisible by 5 and 7', () => {
+        const input = '35'
+        const output = 'BarQixFooBar'
         expect(compute(input)).toEqual(output)        
     })
 })
