f292d2febde05c9003b718d451c4148f06ee3e8a
Jimmy Grande <jimmy.grande@outlook.com>
2020-04-23 12:06:52 +0200

    should return Foo when number can be divide by 3: test ok

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // TODO: write the compute function and implement
 // all rules step by step
 const compute = n => {
+    const safeN = parseInt(n, 10)
+    if(safeN % 3 === 0) {
+        return 'Foo'
+    }
     return n
 }
 
