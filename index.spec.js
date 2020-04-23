7f8a91f3e49d4b7d5d78f07c55213b63e23b6db0
Jimmy Grande <jimmy.grande@outlook.com>
2020-04-23 11:06:56 +0200

    should return the string parameter when no rule applies: test ko

--- index.spec.js
+++ index.spec.js
@@ -1,1 +1,10 @@
 // TODO: write necessary unit tests
+const compute = require('./index')
+
+describe('Basic test', () => {
+    it('should return the string passed as a param', () => {
+        const input = '1'
+        const output = '1'
+        expect(compute(input)).toEqual(output)
+    })
+})
