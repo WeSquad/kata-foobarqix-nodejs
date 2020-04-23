b668d428d88ac75933e91c20116279252ca4d66d
Jimmy Grande <jimmy.grande@outlook.com>
2020-04-23 15:28:04 +0200

    should return FooBarQix when value contains 7 AND is divible by 3 and 5: refactor

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 // TODO: write the compute function and implement
 // all rules step by step
 const compute = n => {
-    const safeN = parseInt(n, 10)
+    const map = {
+        3: 'Foo',
+        5: 'Bar',
+        7: 'Qix'
+    }
     let result = ''
-
-    if(safeN % 3 === 0) {
-        result += 'Foo'
-    }
-
-    if(safeN % 5 === 0) {
-        result += 'Bar'
-    }
-
-    if(safeN % 7 === 0) {
-        result += 'Qix'
-    }
+    Object.keys(map).forEach(key => {
+        if(parseInt(n, 10) % key === 0) {
+            result += map[key]
+        } 
+    })
     
     Array.from(n).forEach(v => {
-        if(v === '3') {
-            result += 'Foo'
-        }
-        if(v === '5') {
-            result += 'Bar'
-        }
-        if(v === '7') {
-            result += 'Qix'
-        }
+        Object.keys(map).forEach(key => {
+            if(v === key) {
+                result += map[key]
+            } 
+        })       
     })
     // ternary expression. the double !! convert variable to boolean value
     // the triple === checks for value and type. it is a safer approach
