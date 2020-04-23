78746ea9c19fc18fc2a7728e09e19ceaaec802d2
Jimmy Grande <jimmy.grande@outlook.com>
2020-04-23 12:04:08 +0200

    should return Foo when number can be divide by 3: test ko

--- index.spec.js
+++ index.spec.js
@@ -2,9 +2,17 @@
 const compute = require('./index')
 
 describe('Basic test', () => {
-    it('should return the string passed as a param', () => {
+    it('should return the string passed as a param when no rule match', () => {
         const input = '1'
         const output = '1'
         expect(compute(input)).toEqual(output)
+    })
+})
+
+describe('Implement the divide rule', () => {
+    it('should return Foo if value can be divide by 3', () => {
+        const inputs = ['3', '6', '9', '12']
+        const output = 'Foo'
+        inputs.forEach(input => expect(compute(input)).toEqual(output))        
     })
 })
