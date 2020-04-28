d73f217a52fb6be67e3f42a949881a6d0ee733b1
Jimmy Grande <jimmy.grande@outlook.com>
2020-04-28 15:23:07 +0200

    refactor code and unit test after review

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-// TODO: write the compute function and implement
-// all rules step by step
-const compute = n => {
-    const map = {
-        3: 'Foo',
-        5: 'Bar',
-        7: 'Qix'
-    }
-    let result = ''
-    Object.keys(map).forEach(key => {
-        if(parseInt(n, 10) % key === 0) {
-            result += map[key]
-        } 
-    })
-    
-    Array.from(n).forEach(v => {             
-        Object.keys(map).forEach(key => {
-            if(v === key) {
-                result += map[key]
-            } 
-        })
-        if(v === '0' && !!result) {
-            result += '*'            
-        }
-    })
-    // ternary expression. the double !! convert variable to boolean value
-    // the triple === checks for value and type. it is a safer approach
-    return !!result === true ? result : n.replace('0','*')
-}
+const compose = (...func) => (args) =>
+  func.reduceRight((arg, fn) => fn(arg), args);
 
-module.exports = compute
+const map = {
+  0: "*",
+  3: "Foo",
+  5: "Bar",
+  7: "Qix",
+};
+
+const compute = (n) =>
+  compose(
+    (computedString) => computedString || n.replace("0", "*"),
+    computeReplaceRule(n),
+    computeDivideRule
+  )(n);
+
+const mapDivideRule = (num) => ([divider, value]) =>
+  parseInt(num, 10) % divider === 0 ? value : "";
+const computeDivideRule = (num) =>
+  Object.entries(map).map(mapDivideRule(num)).join("");
+
+const mapReplaceRule = (rules, computedDivision) => (numChar) =>
+  Object.entries(rules)
+    .map(([num, value]) =>
+      (computedDivision && num === "0" && num == numChar) ||
+      (numChar === num && num !== "0")
+        ? value
+        : ""
+    )
+    .join("");
+const computeReplaceRule = (num) => (computedDivision) =>
+  computedDivision +
+  Array.from(num).map(mapReplaceRule(map, computedDivision)).join("");
+
+module.exports = {
+  compute,
+  computeDivideRule,
+  computeReplaceRule,
+};
--- index.spec.js
+++ index.spec.js
@@ -1,88 +1,203 @@
 // TODO: write necessary unit tests
-const compute = require('./index')
+const { compute, computeDivideRule, computeReplaceRule } = require("./index");
 
-describe('Basic test', () => {
-    it('should return the string passed as a param when no rule match', () => {
-        const input = '1'
-        const output = '1'
-        expect(compute(input)).toEqual(output)
-    })
-})
+describe("Compute divide rule", () => {
+  it("should return Foo when number can be divide ONLY by 3", () => {
+    const input = "3";
+    const output = "Foo";
 
-describe('Implement the divide rule', () => {
-    it('should return Foo if value can be divide by 3', () => {
-        const inputs = ['6', '9', '12']
-        const output = 'Foo'
-        inputs.forEach(input => expect(compute(input)).toEqual(output))        
-    })
+    const result = computeDivideRule(input);
 
-    it('should return Qix if value can be divide by 7', () => {
-        const inputs = ['49']
-        const output = 'Qix'
-        inputs.forEach(input => expect(compute(input)).toEqual(output))        
-    })
+    expect(result).toEqual(output);
+  });
 
-    it('should return FooQix if value can be divide by 3 and 7', () => {
-        const input = '21'
-        const output = 'FooQix'
-        expect(compute(input)).toEqual(output) 
-    })  
-})
+  it("should return Bar when number can be divide ONLY by 5", () => {
+    const input = "5";
+    const output = "Bar";
 
-describe('Implement the `contains` rule with the divide rule', () => {
-    it('should return Foo if value contains 3 AND has no divide rule', () => {
-        const input = '13'
-        const output = 'Foo'
-        expect(compute(input)).toEqual(output)        
-    })
+    const result = computeDivideRule(input);
 
-    it('should return FooFoo if value contains 3 AND is divisible by 3', () => {
-        const input = '3'
-        const output = 'FooFoo'
-        expect(compute(input)).toEqual(output)        
-    })
+    expect(result).toEqual(output);
+  });
 
-    it('should return BarBar if value contains 5 AND is divisible by 5', () => {
-        const input = '5'
-        const output = 'BarBar'
-        expect(compute(input)).toEqual(output)        
-    })
+  it("should return Qix when number can be divide ONLY by 7", () => {
+    const input = "7";
+    const output = "Qix";
 
-    it('should return FooBarBar if value contains 5 AND is divisible by 3 and 5', () => {
-        const input = '15'
-        const output = 'FooBarBar'
-        expect(compute(input)).toEqual(output)        
-    })
+    const result = computeDivideRule(input);
 
-    it('should return BarQixFooBar if value contains 3 and 5 AND is divisible by 5 and 7', () => {
-        const input = '35'
-        const output = 'BarQixFooBar'
-        expect(compute(input)).toEqual(output)        
-    })
+    expect(result).toEqual(output);
+  });
 
-    it('should return Qix if value contains 7 AND has no divide rule', () => {
-        const input = '71'
-        const output = 'Qix'
-        expect(compute(input)).toEqual(output)
-    })
+  it("should return FooBar when number can be divide ONLY by 3 AND 5", () => {
+    const input = "15";
+    const output = "FooBar";
 
-    it('should return FooQix if value contains 7 AND is divible by 3', () => {
-        const input = '72'
-        const output = 'FooQix'
-        expect(compute(input)).toEqual(output)
-    })
-})
+    const result = computeDivideRule(input);
 
-describe('Replace 0 with *', () => {
-    it('should replace 0 with a * and applies other rules', () => {
-        const mock = {
-            '101': '1*1',
-            '303': 'FooFoo*Foo', 
-            '105': 'FooBarQix*Bar', 
-            '10101': 'FooQix**'
-        }
-        Object.entries(mock).forEach(([input, output]) => {
-            expect(compute(input)).toEqual(output)
-        })
-    })
-})
+    expect(result).toEqual(output);
+  });
+
+  it("should return FooQix when number can be divide ONLY by 3 AND 7", () => {
+    const input = "21";
+    const output = "FooQix";
+
+    const result = computeDivideRule(input);
+
+    expect(result).toEqual(output);
+  });
+
+  it("should return BarQix when number can be divide ONLY by 5 AND 7", () => {
+    const input = "35";
+    const output = "BarQix";
+
+    const result = computeDivideRule(input);
+
+    expect(result).toEqual(output);
+  });
+
+  it("should return FooBarQix when number can be divide ONLY by 3 AND 5 AND 7", () => {
+    const input = "105";
+    const output = "FooBarQix";
+
+    const result = computeDivideRule(input);
+
+    expect(result).toEqual(output);
+  });
+});
+
+describe("Compute replace rule", () => {
+  it("should replace 3 by Foo", () => {
+    const input = "3";
+    const output = "Foo";
+
+    const result = computeReplaceRule(input)("");
+
+    expect(result).toEqual(output);
+  });
+
+  it("should replace 5 by Bar", () => {
+    const input = "51";
+    const output = "Bar";
+
+    const result = computeReplaceRule(input)("");
+
+    expect(result).toEqual(output);
+  });
+
+  it("should replace 7 by Qix", () => {
+    const input = "171";
+    const output = "Qix";
+
+    const result = computeReplaceRule(input)("");
+
+    expect(result).toEqual(output);
+  });
+
+  it("should replace 3 by Foo AND 5 by Bar AND 7 by Qix", () => {
+    const input = "111311151117111";
+    const output = "FooBarQix";
+
+    const result = computeReplaceRule(input)("");
+
+    expect(result).toEqual(output);
+  });
+});
+
+describe("Compute number with all rules", () => {
+  it("should return the string passed as a param when no rule match", () => {
+    const input = "1";
+    const output = "1";
+
+    const result = compute(input);
+
+    expect(result).toEqual(output);
+  });
+
+  it("should return Foo if value can be divide by 3", () => {
+    const inputs = ["6", "9", "12"];
+    const output = "Foo";
+    inputs.forEach((input) => {
+      const result = compute(input);
+      expect(result).toEqual(output);
+    });
+  });
+
+  it("should return Qix if value can be divide by 7", () => {
+    const inputs = ["49"];
+    const output = "Qix";
+    inputs.forEach((input) => expect(compute(input)).toEqual(output));
+  });
+
+  it("should return FooQix if value can be divide by 3 and 7", () => {
+    const input = "21";
+    const output = "FooQix";
+    const result = compute(input);
+    expect(result).toEqual(output);
+  });
+
+  it("should return Foo if value contains 3 AND has no divide rule", () => {
+    const input = "13";
+    const output = "Foo";
+    const result = compute(input);
+    expect(result).toEqual(output);
+  });
+
+  it("should return FooFoo if value contains 3 AND is divisible by 3", () => {
+    const input = "3";
+    const output = "FooFoo";
+    const result = compute(input);
+    expect(result).toEqual(output);
+  });
+
+  it("should return BarBar if value contains 5 AND is divisible by 5", () => {
+    const input = "5";
+    const output = "BarBar";
+    const result = compute(input);
+    expect(result).toEqual(output);
+  });
+
+  it("should return FooBarBar if value contains 5 AND is divisible by 3 and 5", () => {
+    const input = "15";
+    const output = "FooBarBar";
+    const result = compute(input);
+    expect(result).toEqual(output);
+  });
+
+  it("should return BarQixFooBar if value contains 3 and 5 AND is divisible by 5 and 7", () => {
+    const input = "35";
+    const output = "BarQixFooBar";
+    const result = compute(input);
+    expect(result).toEqual(output);
+  });
+
+  it("should return Qix if value contains 7 AND has no divide rule", () => {
+    const input = "71";
+    const output = "Qix";
+    const result = compute(input);
+    expect(result).toEqual(output);
+  });
+
+  it("should return FooQix if value contains 7 AND is divible by 3", () => {
+    const input = "72";
+    const output = "FooQix";
+    const result = compute(input);
+    expect(result).toEqual(output);
+  });
+
+  it("should replace 0 with a * and applies other rules", () => {
+    // Arrange
+    const mock = {
+      "101": "1*1",
+      "303": "FooFoo*Foo",
+      "105": "FooBarQix*Bar",
+      "10101": "FooQix**",
+    };
+    Object.entries(mock).forEach(([input, output]) => {
+      // Act
+      const result = compute(input);
+      // Assert
+      expect(result).toEqual(output);
+    });
+  });
+});
